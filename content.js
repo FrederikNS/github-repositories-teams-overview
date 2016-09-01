aa06efb335cab998c4dacfb28168de977ad7b503
Frederik Nordahl Jul Sabroe <frederikns@gmail.com>
2016-09-01 19:43:03 +0200

    Added initial working version.

---
+++ content.js
@@ -0,0 +1,55 @@
+knownTeams = {}
+
+function enrichTeam(el, response) {
+  if (response.length > 0) {
+    teamNames = response.map(function(team) {return `<a href="https://github.com/orgs${window.location.pathname}/teams/${team.slug}">${team.name}</a>`})
+
+    var teamElement = document.createElement('div')
+    teamElement.style.color = "#666"
+    teamElement.innerHTML = `Teams: ${teamNames.join(', ')}`
+
+    descriptionNode = el.querySelector('.repo-list-meta')
+    descriptionNode.parentNode.insertBefore(teamElement, descriptionNode)
+  }
+}
+
+function enrichTeams() {
+  chrome.storage.sync.get(function(items) {
+    if (items.personalAccessToken) {
+      var repos = document.querySelectorAll('.repo-list>.repo-list-item')
+
+      repos.forEach(function(el, i) {
+        var repoName = el.querySelector('.repo-list-name>a').pathname
+        if (knownTeams.hasOwnProperty(repoName)) {
+          enrichTeam(el, knownTeams[repoName])
+        } else {
+          qwest.get(`https://api.github.com/repos${repoName}/teams`, null, {
+            headers: {
+              Accept: "application/vnd.github.v3+json",
+              Authorization: `token ${items.personalAccessToken}`
+            }
+          }).then(function(xhr, response) {
+            knownTeams[repoName] = response
+            enrichTeam(el, response)
+          })
+        }
+      })
+    }
+  })
+}
+enrichTeams()
+var repoList = document.querySelector('.repo-list')
+var observer = new MutationObserver(enrichTeams)
+observer.observe(repoList, {childList: true})
+//
+// enrichTeams()
+//
+// document.addEventListener('pjax:end', function() {
+//   console.log('event fired')
+//   enrichTeams()
+// })
+// document.querySelectorAll('.repo-list')[0].addEventListener('', function() {
+//   console.log('event fired')
+//
+//   enrichTeams()
+// })
---
+++ options.js
@@ -0,0 +1,37 @@
+// Saves options to chrome.storage
+function save_options() {
+  var personalAccessToken = document.getElementById('personal-access-token').value;
+  chrome.storage.sync.set({
+    personalAccessToken: personalAccessToken
+  }, function() {
+    var status = document.getElementById('status');
+    status.textContent = 'Checking login...';
+    qwest.get('https://api.github.com/user', null, {
+      headers: {
+        Accept: "application/vnd.github.v3+json",
+        Authorization: "token " + personalAccessToken
+      }
+    }).then(function(xhr, response) {
+      var scopes = xhr.getResponseHeader("X-OAuth-Scopes").split(', ')
+      if (scopes.indexOf('repo') != -1) {
+        status.textContent = "Logged in!"
+      } else {
+        status.textContent = "Login failed! The personal access token is missing the \"repo\" scope."
+      }
+    }).catch(function(e, xhr, response) {
+      status.textContent = xhr.statusText
+    })
+  });
+}
+
+// Restores select box and checkbox state using the preferences
+// stored in chrome.storage.
+function restore_options() {
+  chrome.storage.sync.get({
+    personalAccessToken: ""
+  }, function(items) {
+    document.getElementById('personal-access-token').value = items.personalAccessToken;
+  });
+}
+document.addEventListener('DOMContentLoaded', restore_options);
+document.getElementById('save').addEventListener('click', save_options);
